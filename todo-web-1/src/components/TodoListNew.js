c581a5e47bc2528d1a6c08124e269d8e132e3466
Nick Larew <nick.larew@10gen.com>
2019-05-13 22:38:51 -0400

    [todo-web] Base To-Do Functionality

--- todo-web-1/src/components/TodoListNew.js
+++ todo-web-1/src/components/TodoListNew.js
@@ -1,150 +1,195 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import { useStitchAuth } from "./StitchAuth";
-import {app, items} from "../stitch";
+import { app, items } from "../stitch";
 
 const todoReducer = (state, { type, payload }) => {
-   switch(type) {
-      case "setTodos": {
-         return {
-            ...state,
-            todos: payload.todos || []
-         }
+  switch (type) {
+    case "setTodos": {
+      return {
+        ...state,
+        todos: payload.todos || []
+      };
+    }
+    case "addTodo": {
+      return {
+        ...state,
+        todos: [...state.todos, payload]
+      };
+    }
+    case "removeTodo": {
+      const removeSpecifiedTodo = todo => todo.id !== payload.id;
+      return {
+        ...state,
+        todos: state.todos.filter(removeSpecifiedTodo)
+      };
+    }
+    case "clearCompletedTodos": {
+      const isNotCompleted = todo => todo.checked !== true
+      return {
+        ...state,
+        todos: state.todos.filter(isNotCompleted)
+      };
+    }
+    case "clearTodos": {
+      return { ...state, todos: [] };
+    }
+    case "setTodoStatus": {
+      const updateTodoStatus = todo => {
+        const isThisTodo = todo._id === payload.id;
+        return isThisTodo ? { ...todo, status: payload.status } : todo;
       }
-      case "addTodo": {
-         return {
-            ...state,
-            todos: state.todos.push(payload)
-         }
+      return {
+        ...state,
+        todos: state.todos.map(updateTodoStatus)
+      };
+    }
+    case "toggleTodoStatus": {
+      const updateStatus = todo => {
+        const isThisTodo = todo._id === payload.id;
+        return isThisTodo ? { ...todo, checked: !todo.checked } : todo;
       }
-      case "removeTodo": {
-         const removeSpecifiedTodo = todo => todo.id !== payload.id
-         return {
-           ...state,
-           todos: state.todos.filter(removeSpecifiedTodo)
-         };
-      }
-      case "clearTodos": {
-         return { ...state, todos: [] }
-      }
-      case "setTodoStatus": {
-         const updateTodoStatus = todo => todo.id !== payload.id
-            ? todo
-            : { ...todo, status: payload.status }
-         return {
-            ...state,
-            todos: state.todos.map(updateTodoStatus)
-         }
-      }
-      default: {}
-   }
-}
+      return {
+        ...state,
+        todos: state.todos.map(updateStatus)
+      };
+    }
+    default: {
+    }
+  }
+};
 
 function useTodoItems(userId) {
-   const [state, dispatch] = React.useReducer(todoReducer, { todos: [] });
-   
-   // Todo Actions
-   const loadTodos = async () => {
-      const todos = await items.find({}, {limit: 1000}).asArray()
-      dispatch({ type: "setTodos", payload: { todos } })
-   }
-   const addTodo = async (text) => {
-      const todo = { text, owner_id: userId }
-      const result = await items.insertOne(todo)
-      dispatch({ type: "addTodo", payload: { ...todo, _id: result.insertedId } })
-   }
-   const removeTodo = async (todoId) => {
-      await items.deleteOne({ _id: todoId })
-      dispatch({ type: "removeTodo", payload: { id: todoId } })
-   }
-   const clearTodos = async () => {
-      await items.deleteMany({});
-      dispatch({ type: "clearTodos" })
-   }
-   const setTodoStatus = async (id, status) => {
-      await items.updateOne(
-         { _id: id },
-         { $set: { checked: status } },
-         { returnNewDocument: true }
-      )
-      dispatch({ type: "setTodoStatus", payload: { id, status } })
-   }
+  const [state, dispatch] = React.useReducer(todoReducer, { todos: [] });
 
-   React.useEffect(() => {
-      loadTodos()
-   }, []);
+  // Todo Actions
+  const loadTodos = async () => {
+    const todos = await items.find({}, { limit: 1000 }).asArray();
+    dispatch({ type: "setTodos", payload: { todos } });
+  };
+  const addTodo = async text => {
+    const todo = { text, owner_id: userId };
+    const result = await items.insertOne(todo);
+    dispatch({ type: "addTodo", payload: { ...todo, _id: result.insertedId } });
+  };
+  const removeTodo = async todoId => {
+    await items.deleteOne({ _id: todoId });
+    dispatch({ type: "removeTodo", payload: { id: todoId } });
+  };
+  const clearTodos = async () => {
+    await items.deleteMany({});
+    dispatch({ type: "clearTodos" });
+  };
+  const clearCompletedTodos = async () => {
+    await items.deleteMany({ checked: true });
+    dispatch({ type: "clearCompletedTodos" });
+  };
+  const setTodoCompletionStatus = async (todoId, status) => {
+    await items.updateOne(
+      { _id: todoId },
+      { $set: { checked: status } },
+      { returnNewDocument: true }
+    );
+    dispatch({ type: "setTodoStatus", payload: { todoId, status } });
+  };
+  const toggleTodoStatus = async (todoId) => {
+    const todo = state.todos.find(t => t._id === todoId);
+    await items.updateOne(
+      { _id: todoId },
+      { $set: { checked: !todo.currentStatus } },
+      { returnNewDocument: true }
+    );
+    dispatch({ type: "toggleTodoStatus", payload: { id: todoId } });
+  };
 
-   return {
-      items: state.todos,
-      actions: { addTodo, removeTodo, setTodoStatus, clearTodos }
-   }
+  React.useEffect(() => {
+    loadTodos();
+  }, []);
+
+  return {
+    items: state.todos,
+    actions: {
+      addTodo,
+      removeTodo,
+      setTodoCompletionStatus,
+      clearTodos,
+      clearCompletedTodos,
+      toggleTodoStatus,
+    }
+  };
 }
 
 function TodoControls(props) {
-   const items = props.items || []
-   return (
-      <div className="controls">
-         <input
-            type="text"
-            className="new-item"
-            placeholder="Add a new item and hit <enter>"
-            onKeyDown={e => this.addItem(e)}
-         />
-         {items.filter(x => x.checked).length > 0
-            ? <div
-               href=""
-               className="cleanup-button"
-               onClick={() => this.clear()}
-            >
-               delete selected item(s)
-            </div>
-            : null}
-      </div>
-   )
+  const { items, actions } = props;
+  const [inputText, setInputText] = useState("");
+  const handleInput = e => setInputText(e.target.value);
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      if(inputText) {
+        actions.addTodo(inputText);
+        setInputText("")
+      }
+    }
+  };
+  return (
+    <div className="controls">
+      <input
+        type="text"
+        className="new-item"
+        placeholder="Add a new item and hit <enter>"
+        onChange={handleInput}
+        onKeyDown={handleKeyPress}
+        value={inputText}
+      />
+      {items && items.filter(x => x.checked).length > 0 ? (
+        <button
+          className="cleanup-button"
+          onClick={actions.clearCompletedTodos}
+        >
+          Clear Completed
+        </button>
+      ) : null}
+    </div>
+  );
 }
 
 function TodoList(props) {
-   const { items, actions } = useTodoItems();
-   // Calculated Properties
-   const hasItems = items.length > 0;
-   // Sub-Components
-   const EmptyList = () => (
-     <li>
-       <div className="list-empty-label">No Todo Items :(</div>
-     </li>
-   );
-
-   return (
-     <ul className="items-list">
-       {!hasItems ? (
-         <EmptyList />
-       ) : (
-         items.map(item => (
-           <TodoItem
-             key={item._id.toString()}
-             item={item}
-             items={this.items}
-             onChange={() => this.loadList()}
-             onStartChange={() => this.setPending()}
-           />
-         ))
-       )}
-     </ul>
-   );
+  const { items, actions } = props;
+  // Calculated Properties
+  return items.length === 0 ? (
+    <div>No Todo Items :(</div>
+  ) : (
+    <ul className="items-list">
+      {items.map(item => (
+        <TodoItem
+          key={item._id.toString()}
+          item={item}
+          remove={() => actions.removeTodo(item._id)}
+          toggleStatus={() => actions.toggleTodoStatus(item._id)}
+          setStatus={status =>
+            actions.setTodoCompletionStatus(item._id, status)
+          }
+        />
+      ))}
+    </ul>
+  );
 }
 
 function TodoApp(props) {
-   const { currentUser, actions } = useStitchAuth();
-   const todo = useTodoItems(currentUser.id);
-   return (
-      <div>
-         <button onClick={actions.handleLogout}>logout</button>
-         <TodoControls />
-         <TodoList
-            items={todo.items}
-         />
-      </div>
-   )
+  // const auth = useStitchAuth();
+  // const todo = useTodoItems(auth.currentUser.id);
+  const {
+    currentUser,
+    actions: { handleLogout }
+  } = useStitchAuth();
+  const todo = useTodoItems(currentUser.id);
+  return (
+    <div>
+      <button onClick={handleLogout}>logout</button>
+      <TodoControls items={todo.items} actions={todo.actions} />
+      <TodoList items={todo.items} actions={todo.actions} />
+    </div>
+  );
 }
 
 export default TodoApp;
